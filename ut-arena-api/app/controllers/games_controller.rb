44d5361e60c3786801c2e7cc8f48bccfaf0916b9
ksd <krzysztof.szyda@blstream.com>
2016-02-05 08:00:44 +0100

    Game list view, first solution

--- ut-arena-api/app/controllers/games_controller.rb
+++ ut-arena-api/app/controllers/games_controller.rb
@@ -3,4 +3,22 @@ class GamesController < ApplicationController
     games = Game.all
     render json: games, status: 200
   end
+
+  def show
+    game = Game.find(params[:id])
+    render json: game,
+      except: [:created_at, :updated_at],
+      include:
+        {
+          players:
+         {
+           only: [:nick, :id],
+           include: {
+             player_games:
+               { only: [:score, :team] }
+             }
+         }
+       },
+      status: 200
+  end
 end
