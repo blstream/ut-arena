253a0270ef28a29e63eaf4f20e91015f7d3b59f8
ksd <krzysztof.szyda@blstream.com>
2016-02-05 10:57:12 +0100

    Create game feature added

--- ut-arena-api/app/controllers/games_controller.rb
+++ ut-arena-api/app/controllers/games_controller.rb
@@ -7,4 +7,18 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
     @players = @game.players
   end
+
+  def create
+    @game = Game.create(game_params)
+    @game.save
+    redirect_to @game
+  end
+
+
+  private
+
+  def game_params
+    params.require(:game).permit(:start_date, :time_limit, :map_name, :match_type)
+  end
+
 end
