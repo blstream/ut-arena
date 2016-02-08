e777c0e8895e9740b1924a0cfe952b7c4d9be9d5
ksd <krzysztof.szyda@blstream.com>
2016-02-08 11:34:46 +0100

    User model and email validation created

--- ut-arena-api/db/schema.rb
+++ ut-arena-api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204092900) do
+ActiveRecord::Schema.define(version: 20160208092700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 20160204092900) do
     t.string   "nick"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "login"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
