d7550371953fd37cc3e15934d5828058a01f3904
ksd <krzysztof.szyda@blstream.com>
2016-02-09 15:38:55 +0100

    Token expires after one day now, user cannot login with expired token

--- ut-arena-api/db/migrate/20160208092700_create_users.rb
+++ ut-arena-api/db/migrate/20160208092700_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :password_digest
       t.string :authentication_token
+      t.timestamp :token_expires_at
       t.timestamps null: false
     end
   end
