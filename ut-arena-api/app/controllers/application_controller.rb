9f411dc7ee01ba95cb3f1daaed6668c3d28b0df3
ksd <krzysztof.szyda@blstream.com>
2016-02-10 12:13:15 +0100

    Authentication enabled again

--- ut-arena-api/app/controllers/application_controller.rb
+++ ut-arena-api/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  before_action :destroy_session
+  before_action :destroy_session, :authenticate_user!
 
   def destroy_session
     request.session_options[:skip] = true
