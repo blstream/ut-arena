a2813d1f54e3260558d18c3a7525e99ea2556978
ksd <krzysztof.szyda@blstream.com>
2016-02-22 18:27:56 +0100

    Commenting and standardizing the Capistrano code

---
+++ ut-arena-api/config/deploy/stage.rb.example.rb
@@ -0,0 +1,43 @@
+# This example is for the Production stage
+
+# Here the stage name is set, it would later be executed with command like:
+# cap production deploy
+set :stage, :development
+
+# User that would be used is set to the production
+set :user, "development"
+
+# The server address is defined here
+server_address = "192.168.33.10"
+
+# Here are defined roles for the production server
+role :app, "#{fetch :user}@#{server_address}"
+role :web, "#{fetch :user}@#{server_address}"
+role :db,  "#{fetch :user}@#{server_address}"
+
+# Path where would the application be deployed
+set :deploy_to, "/home/development/apps/#{fetch :application}"
+
+# Server settings
+server server_address, user: (fetch :user), roles: %w{web app}
+
+
+# Stage specified deploy tasks should be defined here, like the example below
+#
+# namespace :deploy do
+#
+# # The description of the task
+#   desc 'Setup API'
+# # Task name
+#   task :setup do
+# # On what roles would the task be executed
+#     on roles(:app), in: :sequence, wait: 5 do
+#       within release_path do
+#         # here are commands to be executed or other operations that you need to do
+#         # for example rake db:setup for API
+#         # execute :rake, "-f #{release_path}/ut-arena-api/Rakefile db:setup"
+#       end
+#     end
+#   end
+#
+# end
