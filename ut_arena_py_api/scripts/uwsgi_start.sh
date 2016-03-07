d1b195724334a970673d11c7db7899e91f5809d1
ksd <krzysztof.szyda@blstream.com>
2016-03-07 13:40:15 +0100

    Changes in nginx conf and uwsgi start script

--- ut_arena_py_api/scripts/uwsgi_start.sh
+++ ut_arena_py_api/scripts/uwsgi_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -u
 
 PROFILE=$1
-APP_HOME=/opt/ut-arena
+APP_HOME=/opt/ut-arena/ut_arena_py_api
 
 exec uwsgi --socket ut-arena.sock --chdir=/opt/ut-arena --module ut_arena.wsgi:application --processes 2 -t 60 --disable-logging -M --need-app -b 32768 --chmod-socket=666
