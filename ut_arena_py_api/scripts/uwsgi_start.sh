c44cfd70593a60c335fdb6b89210a2c633494ea2
ksd <krzysztof.szyda@blstream.com>
2016-03-10 13:06:00 +0100

    uwsgi start script app home modified

--- ut_arena_py_api/scripts/uwsgi_start.sh
+++ ut_arena_py_api/scripts/uwsgi_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -u
 
-APP_HOME=/opt/ut-arena/ut_arena_py_api
+APP_HOME=/opt/ut-arena/
 
 exec uwsgi --socket ut-arena.sock --chdir=/opt/ut-arena/ --module ut_arena.wsgi:application --processes 2 -t 60 --disable-logging -M --need-app -b 32768 --chmod-socket=666
