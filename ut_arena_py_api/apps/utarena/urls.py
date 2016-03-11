a7ef3f7b21732e3bbeac11dcdc081875aca3707b
ksd <krzysztof.szyda@blstream.com>
2016-03-11 11:44:07 +0100

    pyc filed removed, project reformated, login url and player model changed

--- ut_arena_py_api/apps/utarena/urls.py
+++ ut_arena_py_api/apps/utarena/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include, url
 from rest_framework.authtoken import views
 
 urlpatterns = [
-  url(r'^', include('apps.api_test.urls')),
-  url(r'^api-token-auth/', views.obtain_auth_token),
+    url(r'^', include('apps.api_test.urls')),
+    url(r'^login/', views.obtain_auth_token),
 ]
